3976546ea01383e743d2a64f8a3757e9ebc0f766
StefanHadzhiev <stefanhadzhiev24@gmail.com>
2024-11-13 08:24:42 +0200

    Creating a cities context and consuming it.

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -4,7 +4,11 @@ import Spinner from "./Spinner";
 import Message from "./Message";
 import CityItem from "./CityItem";
 
-function CityList({ cities, isLoading }) {
+import { useCities } from "../contexts/CitiesContext";
+
+function CityList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) {
     return <Spinner />;
   }
